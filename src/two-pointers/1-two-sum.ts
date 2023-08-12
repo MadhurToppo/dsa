b11118071625783156c432d4bc7d2cd497773f3e
Madhur Toppo <madhur.toppo@gmail.com>
2023-08-12 23:13:33 +0530

    Add two-sum problem solution

---
+++ src/two-pointers/1-two-sum.ts
@@ -0,0 +1,20 @@
+const getTwoSum = (nums: Array<number>, target: number) => {
+  let left: number = 0;
+  let right: number = nums.length;
+
+  while (left < right) {
+    const sum = nums[left] + nums[right];
+    if (sum === target) {
+      return [left, right];
+    }
+    if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return [-1, -1];
+};
+
+console.log(getTwoSum([1, 2, 3, 4, 6], 6));
+console.log(getTwoSum([2, 5, 9, 11], 11));

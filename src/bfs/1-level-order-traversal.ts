d5ce3f003dde71b8e44b19bb9f316c479e6309ce
Madhur Toppo <madhur.toppo@gmail.com>
2023-08-13 01:40:07 +0530

    Add problem bfs level order traversal

---
+++ src/bfs/1-level-order-traversal.ts
@@ -0,0 +1,40 @@
+import TreeNode from "./tree.node";
+
+const levelOrderTraversal = (root: TreeNode): Array<Array<number>> => {
+  let result: Array<Array<number>> = new Array();
+  if (root === null) {
+    return result;
+  }
+
+  const queue: Array<TreeNode> = new Array();
+  queue.push(root);
+
+  while (queue.length > 0) {
+    const levelSize: number = queue.length;
+    let currentLevel: Array<number> = new Array();
+
+    for (let i = 0; i < levelSize; i++) {
+      let currentNode: TreeNode | undefined = queue.shift();
+      currentLevel.push(currentNode!.val);
+      if (currentNode!.left !== null) {
+        queue.push(currentNode!.left);
+      }
+      if (currentNode!.right !== null) {
+        queue.push(currentNode!.right);
+      }
+    }
+
+    result.push(currentLevel);
+  }
+
+  return result;
+};
+
+const root = new TreeNode(12);
+root.left = new TreeNode(7);
+root.right = new TreeNode(1);
+root.left.left = new TreeNode(9);
+root.right.left = new TreeNode(10);
+root.right.right = new TreeNode(5);
+const answer = levelOrderTraversal(root);
+console.log(answer);

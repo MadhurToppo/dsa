b84cd48075b8aba94c4d89f41ec6fb2c3644b100
Madhur Toppo <madhur.toppo@gmail.com>
2023-08-13 17:00:17 +0530

    Add min and max depth of binary tree

---
+++ src/bfs/5-minimum-depth-root-to-leaf.ts
@@ -0,0 +1,41 @@
+import TreeNode from "./tree.node";
+
+const minDepthRootToLeaf = (root: TreeNode): number => {
+  if (root === null) {
+    return 0;
+  }
+
+  const queue: Array<TreeNode> = new Array();
+  queue.push(root);
+  let minDepth = 0;
+
+  while (queue.length > 0) {
+    minDepth++;
+    const levelSize: number = queue.length;
+
+    for (let i = 0; i < levelSize; i++) {
+      let currentNode: TreeNode | undefined = queue.shift();
+      if (currentNode?.left === null && currentNode.right === null) {
+        return minDepth;
+      }
+      if (currentNode!.left !== null) {
+        queue.push(currentNode!.left);
+      }
+      if (currentNode!.right !== null) {
+        queue.push(currentNode!.right);
+      }
+    }
+  }
+  return minDepth;
+};
+
+// Test cases
+const root = new TreeNode(12);
+root.left = new TreeNode(7);
+root.right = new TreeNode(1);
+root.right.left = new TreeNode(10);
+root.right.right = new TreeNode(5);
+console.log(minDepthRootToLeaf(root));
+root.left.left = new TreeNode(9);
+root.right.left.left = new TreeNode(11);
+console.log(minDepthRootToLeaf(root));
---
+++ src/bfs/6-max-depth-tree.ts
@@ -0,0 +1,38 @@
+import TreeNode from "./tree.node";
+
+const maxDepthTree = (root: TreeNode): number => {
+  if (root === null) {
+    return 0;
+  }
+
+  const queue: Array<TreeNode> = new Array();
+  queue.push(root);
+  let maxDepth = 0;
+
+  while (queue.length > 0) {
+    maxDepth++;
+    const levelSize: number = queue.length;
+
+    for (let i = 0; i < levelSize; i++) {
+      let currentNode: TreeNode | undefined = queue.shift();
+      if (currentNode!.left !== null) {
+        queue.push(currentNode!.left);
+      }
+      if (currentNode!.right !== null) {
+        queue.push(currentNode!.right);
+      }
+    }
+  }
+  return maxDepth;
+};
+
+// Test cases
+const root = new TreeNode(12);
+root.left = new TreeNode(7);
+root.right = new TreeNode(1);
+root.right.left = new TreeNode(10);
+root.right.right = new TreeNode(5);
+console.log(maxDepthTree(root));
+root.left.left = new TreeNode(9);
+root.right.left.left = new TreeNode(11);
+console.log(maxDepthTree(root));
